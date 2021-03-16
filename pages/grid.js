128c751fed61e095306fbae6cc4c706a8f23919c
dommarr <dom.marrone@gmail.com>
2021-03-16 17:00:24 -0400

    grid test

---
+++ pages/grid.js
@@ -0,0 +1,18 @@
+export default function Grid() {
+  return (
+    <div className="grid grid-cols-4 grid-rows-3 gap-4">
+      <div className="bg-gray-300 h-40 p-4">title-1</div>
+      <div className="bg-gray-300 h-40 p-4">title-2</div>
+      <div className="bg-gray-300 h-40 p-4">title-3</div>
+      <div className="bg-gray-300 h-40 p-4">title-4</div>
+      <div className="bg-gray-300 h-40 p-4">mid-1</div>
+      <div className="bg-gray-300 h-40 p-4">mid-2</div>
+      <div className="bg-gray-300 h-40 p-4">mid-3</div>
+      <div className="bg-gray-300 h-40 p-4">mid-4</div>
+      <div className="bg-gray-300 h-40 p-4">end-1</div>
+      <div className="bg-gray-300 h-40 p-4">end-2</div>
+      <div className="bg-gray-300 h-40 p-4">end-3</div>
+      <div className="bg-gray-300 h-40 p-4">end-4</div>
+    </div>
+  );
+}
