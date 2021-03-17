6c4eb0b74c218f1ff370ff8ff01b5274ff2a49df
dommarr <dom.marrone@gmail.com>
2021-03-17 16:58:42 -0400

    set tailwind config important

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,7 @@ const colors = require("tailwindcss/colors");
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
+  important: true,
   theme: {
     colors: {
       transparent: "transparent",
